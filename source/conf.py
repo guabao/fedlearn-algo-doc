099b4aee7973ca616cb71f0c396f3090858508b4
jiazhou wang <jiazhou.wang@gmail.com>
2021-08-12 16:34:20 -0700

    fix ext

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,7 @@ release = 'alpha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'numpydoc'
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
